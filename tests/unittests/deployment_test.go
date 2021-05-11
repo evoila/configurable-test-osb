8bfbf9ec453c5f9f90228b9a5888c34b3d520f41
MaxFuhrich <maxfuhrich@gmail.com>
2021-05-11 11:17:10 +0200

    -restructured test into multiple test functions that are more granular

--- tests/unittests/deployment_test.go
+++ tests/unittests/deployment_test.go
@@ -5,11 +5,55 @@ import (
 	"testing"
 )
 
-func TestNewServiceDeployment(t *testing.T) {
+func TestNewServiceDeploymentReturnsCorrectValues(t *testing.T) {
 	instanceID := "IAmInstance"
 	settings := model.Settings{
 		ProvisionSettings: model.ProvisionSettings{
 			CreateDashboardURL: false,
+			CreateMetadata:     false,
+		},
+	}
+	plan := model.ServicePlan{
+		ID: "PlanOne",
+	}
+	instancesRetrievable := false
+	planSlice := []*model.ServicePlan{&plan}
+	offering := model.ServiceOffering{
+		ID:                   "OfferingOne",
+		Plans:                &planSlice,
+		InstancesRetrievable: &instancesRetrievable,
+	}
+
+	orgGUID := "TestOrg"
+	spaceGUID := "SpaceGUID"
+	offeringSlice := []*model.ServiceOffering{&offering}
+	catalog := model.Catalog{ServiceOfferings: &offeringSlice}
+	request := model.ProvideServiceInstanceRequest{
+		ServiceID:        &offering.ID,
+		PlanID:           &plan.ID,
+		OrganizationGUID: &orgGUID,
+		SpaceGUID:        &spaceGUID,
+	}
+	deployment, _ := model.NewServiceDeployment(instanceID, &request, &settings, &catalog)
+	if deployment == nil {
+		t.Fatalf("Expected object for deployment, got %v", deployment)
+	}
+	if deployment.DashboardURL() != nil {
+		t.Errorf("Expected deployment.DashboardURL() == nil but got %v", deployment.DashboardURL())
+	}
+	if deployment.Metadata() != nil {
+		t.Errorf("Expected deployment.Metadata() == nil but got %v", deployment.Metadata())
+	}
+	if deployment.FetchResponse() != nil {
+		t.Errorf("Expected deployment.FetchResponse() == nil but got %v", deployment.FetchResponse())
+	}
+}
+
+func TestNewServiceDeploymentWithDashboardURL(t *testing.T) {
+	instanceID := "IAmInstance"
+	settings := model.Settings{
+		ProvisionSettings: model.ProvisionSettings{
+			CreateDashboardURL: true,
 			CreateMetadata:     false,
 		},
 	}
@@ -33,49 +77,85 @@ func TestNewServiceDeployment(t *testing.T) {
 		OrganizationGUID: &orgGUID,
 		SpaceGUID:        &spaceGUID,
 	}
-	var deployment *model.ServiceDeployment
-	var operationID *string
-	for i := 0; i < 2; i++ {
-		for j := 0; j < 2; j++ {
-			for k := 0; k < 2; k++ {
-				deployment, operationID = model.NewServiceDeployment(instanceID, &request, &settings, &catalog)
-				if deployment == nil {
-					t.Errorf("Expected deployment to not be nil, but got nil")
-					return
-				}
-				if operationID == nil {
-					t.Errorf("Expected operationID to not be nil, but got nil")
-					return
-				}
-				if settings.ProvisionSettings.CreateDashboardURL && deployment.DashboardURL() == nil {
-					t.Errorf("Expected deployment to have a value for DashboardURL but got nil")
-					return
-				}
-				if !settings.ProvisionSettings.CreateDashboardURL && deployment.DashboardURL() != nil {
-					t.Errorf("Expected deployment to have nil for DashboardURL but got an actual value")
-					return
-				}
-				if settings.ProvisionSettings.CreateMetadata && deployment.Metadata() == nil {
-					t.Errorf("Expected deployment to have a value for Metadata but got nil")
-				}
-				if !settings.ProvisionSettings.CreateMetadata && deployment.Metadata() != nil {
-					t.Errorf("Expected deployment to have nil for Metadata but got an actual value")
-				}
-				if instancesRetrievable && deployment.FetchResponse() == nil {
-					t.Errorf("Expected deployment to have a value for fetchResponse but got nil")
-				}
-				if !instancesRetrievable && deployment.FetchResponse() != nil {
-					t.Errorf("Expected deployment to have nil for fetchResponse but got an actual value")
-				}
-				instancesRetrievable = !instancesRetrievable
-			}
-			settings.ProvisionSettings.CreateDashboardURL = !settings.ProvisionSettings.CreateDashboardURL
-		}
-		settings.ProvisionSettings.CreateMetadata = !settings.ProvisionSettings.CreateMetadata
+	deployment, _ := model.NewServiceDeployment(instanceID, &request, &settings, &catalog)
+	if deployment == nil {
+		t.Fatalf("Expected object for deployment, got %v", deployment)
+	}
+	if deployment.DashboardURL() == nil {
+		t.Errorf("Expected value for deployment.DashboardURL(), but got nil")
 	}
 }
 
-func TestDoOperation(t *testing.T) {
-	//values := [3]bool{nil, false, true}
+func TestNewServiceDeploymentWithMetadata(t *testing.T) {
+	instanceID := "IAmInstance"
+	settings := model.Settings{
+		ProvisionSettings: model.ProvisionSettings{
+			CreateDashboardURL: false,
+			CreateMetadata:     true,
+		},
+	}
+	plan := model.ServicePlan{
+		ID: "PlanOne",
+	}
+	instancesRetrievable := false
+	planSlice := []*model.ServicePlan{&plan}
+	offering := model.ServiceOffering{
+		ID:                   "OfferingOne",
+		Plans:                &planSlice,
+		InstancesRetrievable: &instancesRetrievable,
+	}
+	orgGUID := "TestOrg"
+	spaceGUID := "SpaceGUID"
+	offeringSlice := []*model.ServiceOffering{&offering}
+	catalog := model.Catalog{ServiceOfferings: &offeringSlice}
+	request := model.ProvideServiceInstanceRequest{
+		ServiceID:        &offering.ID,
+		PlanID:           &plan.ID,
+		OrganizationGUID: &orgGUID,
+		SpaceGUID:        &spaceGUID,
+	}
+	deployment, _ := model.NewServiceDeployment(instanceID, &request, &settings, &catalog)
+	if deployment == nil {
+		t.Fatalf("Expected object for deployment, got %v", deployment)
+	}
+	if deployment.Metadata() == nil {
+		t.Errorf("Expected value for deployment.Metadata(), but got nil")
+	}
+}
 
+func TestNewServiceDeploymentIsRetrievable(t *testing.T) {
+	instanceID := "IAmInstance"
+	settings := model.Settings{
+		ProvisionSettings: model.ProvisionSettings{
+			CreateDashboardURL: false,
+			CreateMetadata:     false,
+		},
+	}
+	plan := model.ServicePlan{
+		ID: "PlanOne",
+	}
+	instancesRetrievable := true
+	planSlice := []*model.ServicePlan{&plan}
+	offering := model.ServiceOffering{
+		ID:                   "OfferingOne",
+		Plans:                &planSlice,
+		InstancesRetrievable: &instancesRetrievable,
+	}
+	orgGUID := "TestOrg"
+	spaceGUID := "SpaceGUID"
+	offeringSlice := []*model.ServiceOffering{&offering}
+	catalog := model.Catalog{ServiceOfferings: &offeringSlice}
+	request := model.ProvideServiceInstanceRequest{
+		ServiceID:        &offering.ID,
+		PlanID:           &plan.ID,
+		OrganizationGUID: &orgGUID,
+		SpaceGUID:        &spaceGUID,
+	}
+	deployment, _ := model.NewServiceDeployment(instanceID, &request, &settings, &catalog)
+	if deployment == nil {
+		t.Fatalf("Expected object for deployment, got %v", deployment)
+	}
+	if deployment.FetchResponse() == nil {
+		t.Errorf("Expected value for deployment.FetchResponse(), but got nil")
+	}
 }
